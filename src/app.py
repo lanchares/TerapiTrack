b6f74b16b325735c103df137d87cdeee0f3a641f
lanchares <albertolancharesdiez1@gmail.com>
2025-03-25 22:07:55 +0100

    SCRUM-3, SCRUM-4, SCRUM-52: Configurado repositorio Gihub, entrono Flask y completado estudio de documentación de Flask

---
+++ src/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "¡Hola, mundo desde TerapiTrack!"
+
+@app.route('/about')
+def about():
+    return "Esta es la página sobre nosotros."
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
