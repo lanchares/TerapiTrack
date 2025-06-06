a15a115a4f598e4d237af5fccc994369cc68f50d
lanchares <albertolancharesdiez1@gmail.com>
2025-06-06 20:47:37 +0200

    Creación del modelo evaluacion. Tarea asociada: TFGTT-23

---
+++ src/modelos/evaluacion.py
@@ -0,0 +1,24 @@
+from app import db
+from datetime import date
+
+class Evaluacion(db.Model):
+    __tablename__ = 'Evaluacion'
+    
+    Ejercicio_Sesion_Id = db.Column(db.Integer, 
+                                   db.ForeignKey('Ejercicio_Sesion.Id'), 
+                                   primary_key=True)
+    Puntuacion = db.Column(db.Integer, nullable=False)
+    Comentarios = db.Column(db.Text)
+    Fecha_Evaluacion = db.Column(db.Date, nullable=False, default=date.today)
+    
+    __table_args__ = (
+        db.CheckConstraint(
+            'Puntuacion >= 1 AND Puntuacion <= 5',
+            name='check_puntuacion_rango'
+        ),
+    )
+    
+    # Relación 1:1 con EjercicioSesion
+    ejercicio_sesion = db.relationship('EjercicioSesion', back_populates='evaluacion')
+    
+   
