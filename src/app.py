0b4ca2f618a45dd15650b2b88b155588a865acaf
lanchares <albertolancharesdiez1@gmail.com>
2025-06-06 18:55:26 +0200

    Modificación de archivo app.py para la probar la conexión. Tarea asociada: TFGTT-55

--- src/app.py
+++ src/app.py
@@ -1,12 +1,37 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///TerapiTrack.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///TerapiTrack.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SECRET_KEY'] = '1234' 
+    
+
+    db = SQLAlchemy()
+
+    # Importar modelos para asegurar que se registran en SQLAlchemy
+    from models.usuario import Usuario
+    from models.paciente import Paciente
+    from models.profesional import Profesional
+
+
+    @app.route('/')
+    def index():
+        return "<h1>TerapiTrack - Backend funcionando</h1>"
+
+    return app
+
+def init_db():
+    """Inicializa la base de datos creando todas las tablas"""
+    app = create_app()
+    with app.app_context():
+        db.drop_all()
+        db.create_all()
+        print("Base de datos inicializada correctamente")
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    print("Tablas creadas correctamente")
+    init_db()
+    app = create_app()
+    app.run(debug=True)
