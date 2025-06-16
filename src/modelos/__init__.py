d6f93d11328fb01cfa3f376e9aeb38c798ce2df0
lanchares <albertolancharesdiez1@gmail.com>
2025-06-17 01:39:23 +0200

    Añadido modificaciones del modelo __init__ que importa todas las tablas a la vez . Tarea asociada: TFGTT-55

--- src/modelos/__init__.py
+++ src/modelos/__init__.py
@@ -4,14 +4,14 @@ from .profesional import Profesional
 from .ejercicio import Ejercicio
 from .sesion import Sesion
 from .evaluacion import Evaluacion
-from .asociaciones import PacienteProfesional, EjercicioProfesional
-from .ejercicio_sesion import EjercicioSesion
+from .asociaciones import Paciente_Profesional, Ejercicio_Profesional
+from .ejercicio_sesion import Ejercicio_Sesion
 from .videoRespuesta import VideoRespuesta
 
 
 __all__ = [
     'Usuario', 'Paciente', 'Profesional', 'Ejercicio',
-    'Sesion', 'EjercicioSesion', 'Evaluacion', 'VideoRespuesta',
-    'PacienteProfesional', 'EjercicioProfesional'
+    'Sesion', 'Ejercicio_Sesion', 'Evaluacion', 'VideoRespuesta',
+    'Paciente_Profesional', 'Ejercicio_Profesional'
 ]
 
