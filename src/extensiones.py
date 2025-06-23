cb731bf9380edb75446884fa62aba1ccb35e4fea
lanchares <albertolancharesdiez1@gmail.com>
2025-06-23 14:10:33 +0200

    Implementar cifrado con bcrypt y creación de sistema de permisos por roles. Tarea asociada: TFGTT-6 y TFGTT-88

--- src/extensiones.py
+++ src/extensiones.py
@@ -1,7 +1,24 @@
 # extensions.py
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
-# Instancias globales sin app
 db = SQLAlchemy()
+bcrypt = Bcrypt()
 login_manager = LoginManager()
+
+def init_extensions(app):
+    """Inicializa todas las extensiones con la app Flask"""
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    
+    # Configurar Flask-Login
+    login_manager.login_view = 'auth.login'
+    login_manager.login_message = 'Por favor inicia sesión para acceder a esta página.'
+    login_manager.login_message_category = 'info'
+    
+    @login_manager.user_loader
+    def load_user(user_id):
+        from src.modelos.usuario import Usuario
+        return Usuario.query.get(int(user_id))
